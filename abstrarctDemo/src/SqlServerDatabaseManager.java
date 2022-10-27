32ba98ce57c89cba93d7d8567a6e569affbc4498
berrakssahin <112779654+berrakssahin@users.noreply.github.com>
2022-10-27 15:19:44 +0300

    Add files via upload

---
+++ abstrarctDemo/src/SqlServerDatabaseManager.java
@@ -0,0 +1,9 @@
+import javax.management.BadAttributeValueExpException;
+
+public class SqlServerDatabaseManager extends BaseDatabaseManager{
+
+@Override
+public void getData() {
+    System.out.println("Veri getirildi: Sql Server");
+    }
+}
